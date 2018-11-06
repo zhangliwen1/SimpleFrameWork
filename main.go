a4b7e2dd0a71f705c6e9242fdd7deb6a3011c683
张丽文 <zhangliwen@hexindai.com>
2018-11-06 08:57:46 +0800

    add middleware

--- main.go
+++ main.go
@@ -1,42 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
-//type middleWareHandler struct {
-//	r *httprouter.Router
-//}
-//
-//
-//func NewMiddleWareHandler(r *httprouter.Router) http.Handler {
-//	m := middleWareHandler{}
-//	m.r = r
-//	return m
-//}
-//
-//func (m middleWareHandler) ServerHTTP(res http.ResponseWriter,req *http.Request) {
-//	// add we need middleware
-//
-//	m.r.ServeHTTP(res,req)
-//}
+
+type middleWareHandler struct { // 继承了路由
+	r *httprouter.Router
+}
+
+func NewMiddleWareHandler(r *httprouter.Router) http.Handler {
+	m := middleWareHandler{}
+	m.r = r
+	return m
+}
+
+// middleWareHandler重写了serverHTTP
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	m.r.ServeHTTP(w, r)
+	println("end")
+}
 
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
 
 	router.POST("/user", addUser)
-	//router.POST("user/:user_name")
+
+	//router.POST("/user/:user_name", Login)
+
 	return router
 }
 
-
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
 func main() {
-	r := RegisterHandlers()
-	//mR := NewMiddleWareHandler(r)
-	http.ListenAndServe(":8000",r)
+	r := RegisterHandlers() //路由注册
+	mh := NewMiddleWareHandler(r) // 重写ServerHttp
+	http.ListenAndServe(":8000", mh)
 }
